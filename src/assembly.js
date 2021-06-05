b600af87845e3925c55eee161acbaf7642e509a1
Bernhard <b_walter@arcor.de>
2021-06-05 18:09:54 +0200

    fixed transparency edges and added black edges

--- src/assembly.js
+++ src/assembly.js
@@ -3,109 +3,147 @@ import { LineSegments2 } from 'three/examples/jsm/lines/LineSegments2.js'
 import { LineSegmentsGeometry } from 'three/examples/jsm/lines/LineSegmentsGeometry.js'
 import { LineMaterial } from 'three/examples/jsm/lines/LineMaterial.js'
 
-
-class Assembly {
-    constructor(data, width, height, edge_color, transparent) {
-        this.shapes = data.shapes;
-        this.width = width;
-        this.height = height;
-        this.mapping = data.mapping;
-        this.tree = data.tree;
-        this.bb = data.bb;
+class ObjectGroup extends THREE.Group {
+    constructor(opacity, edge_color) {
+        super();
+        this.opacity = opacity;
         this.edge_color = edge_color;
-        this.transparent = transparent;
     }
 
-    render_edges(edge_list, lineWidth) {
+    setTransparent = (flag) => {
+        for (var i in this.children) {
+            const side = this.children[i];
+            // only change opacity for the first two mesh objects
+            side.material.opacity = (flag & (i < 2)) ? this.opacity : 1.0;
+            // but change dpethTest for all objects
+            side.material.depthWrite = !flag;
+            side.material.depthTest = !flag;
+            side.material.needsUpdate = true;
+        }
+    }
+
+    setBlackEdges = (flag) => {
+        if (this.children.length > 2) {
+            const edges = this.children[2];
+            const color = flag ? 0x000000 : this.edge_color;
+            edges.material.color = new THREE.Color(color);
+            edges.material.needsUpdate = true;
+        }
+    }
+}
+
+class Assembly {
+    constructor(shapes, width, height, edge_color, transparent, transparent_opacity) {
+        this.shapes = shapes;
+        this.width = width;
+        this.height = height;
+        this.edge_color = edge_color;
+        this.transparent = transparent;
+        this.transparent_opacity = transparent_opacity;
+        this.blackEdges = false;
+        this.delim = '\\';
+        this.groups = {};
+    }
+
+    renderEdges(edge_list, lineWidth) {
         var positions = new Float32Array(edge_list.flat().flat());
 
         const lineGeometry = new LineSegmentsGeometry();
         lineGeometry.setPositions(positions);
 
-        const lineMaterial = new LineMaterial({ color: this.edge_color, linewidth: lineWidth });
+        const lineMaterial = new LineMaterial({
+            color: this.edge_color,
+            linewidth: lineWidth,
+            transparent: true,
+            depthWrite: !this.transparent,
+            depthTest: !this.transparent
+        });
         lineMaterial.resolution.set(this.width, this.height);
 
         var edges = new LineSegments2(lineGeometry, lineMaterial);
+        edges.renderOrder = 999;
         return edges
     }
 
-    render_vertices(vertices) {
+    renderVertices(vertices) {
         return {};
     }
 
-    render_shape(shape, color) {
+    renderShape(shape, color, name) {
         var positions = new Float32Array(shape.vertices.flat());
         var normals = new Float32Array(shape.normals.flat());
 
-        var shape_geometry = new THREE.BufferGeometry();
-        shape_geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-        shape_geometry.setAttribute('normal', new THREE.BufferAttribute(normals, 3));
-        shape_geometry.setIndex(shape.triangles)
-        var group = new THREE.Group()
+        var group = new ObjectGroup(this.transparent_opacity, this.edge_color)
 
-        const shape_material = new THREE.MeshStandardMaterial({
+        var shapeGeometry = new THREE.BufferGeometry();
+        shapeGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+        shapeGeometry.setAttribute('normal', new THREE.BufferAttribute(normals, 3));
+        shapeGeometry.setIndex(shape.triangles)
+
+        const shapeMaterial = new THREE.MeshStandardMaterial({
             color: color,
             polygonOffset: true,
             polygonOffsetFactor: 1,
             polygonOffsetUnits: 1,
             transparent: true,
-            opacity: this.transparent ? 0.4 : 1,
+            opacity: this.transparent ? this.transparent_opacity : 1,
             depthWrite: !this.transparent,
             depthTest: !this.transparent,
         });
 
-        const front_material = shape_material.clone()
-        front_material.side = THREE.FrontSide;
+        const frontMaterial = shapeMaterial.clone()
+        frontMaterial.side = THREE.FrontSide;
 
-        const back_material = shape_material.clone()
-        back_material.side = THREE.BackSide;
+        const backMaterial = shapeMaterial.clone()
+        backMaterial.side = THREE.BackSide;
 
-        const front = new THREE.Mesh(shape_geometry, front_material)
-        const back = new THREE.Mesh(shape_geometry, back_material)
+        const front = new THREE.Mesh(shapeGeometry, frontMaterial)
+        front.name = name;
+        const back = new THREE.Mesh(shapeGeometry, backMaterial)
+        back.name = name;
         group.add(back)
         group.add(front)
 
-        var [edge_list, normals_list] = shape.edges
-        if (edge_list.length > 0) {
-            const edgesGeometry = new THREE.EdgesGeometry(shape_geometry)
-            const lineGeometry = new LineSegmentsGeometry().setPositions(edgesGeometry.attributes.position.array);
-            const lineMaterial = new LineMaterial({ color: 0x000000, linewidth: 3 });
-            lineMaterial.resolution.set(window.innerWidth, window.innerHeight);
-            var wireframe = new LineSegments2(lineGeometry, lineMaterial);
-
-            var wireframe = this.render_edges(edge_list, 1)
+        var [edgeList, normalsList] = shape.edges
+        if (edgeList.length > 0) {
+            var wireframe = this.renderEdges(edgeList, 1)
+            wireframe.name = name;
             group.add(wireframe)
         }
 
-        if (normals_list.length > 0) {
-            var wireframe = this.render_edges(normals_list, 1)
+        if (normalsList.length > 0) {
+            var wireframe = this.renderEdges(normalsList, 1)
             group.add(wireframe)
         }
 
         return group
     }
 
-    _render(shapes) {
+    renderLoop(shapes, path) {
         var group = new THREE.Group();
         if (shapes.loc !== null) {
             group.position.set(...shapes.loc[0])
             group.quaternion.set(...shapes.loc[1]);
         }
 
+        path = path + this.delim + shapes.name
+        this.groups[path] = group
+        group.name = path
+
         for (var shape of shapes.parts) {
             if (shape.parts) {
-                group.add(this._render(shape));
+                group.add(this.renderLoop(shape, path));
             } else {
                 var mesh;
                 switch (shape.type) {
                     case "edges":
-                        mesh = this.render_edges(shape);
+                        mesh = this.renderEdges(shape);
                         break;
                     case "vertices":
-                        mesh = this.render_vertices(shape);
+                        mesh = this.renderVertices(shape);
                         break;
                     default:
-                        mesh = this.render_shape(shape.shape, shape.color);
+                        mesh = this.renderShape(shape.shape, shape.color, shape.name);
                 }
                 group.add(mesh);
             }
@@ -114,7 +152,29 @@ class Assembly {
     }
 
     render() {
-        return this._render(this.shapes);
+        return this.renderLoop(this.shapes, "");
+    }
+
+    setTransparent = (flag) => {
+        this.transparent = flag;
+        for (var path in assembly.groups) {
+            for (var obj of assembly.groups[path].children) {
+                if (obj instanceof ObjectGroup) {
+                    obj.setTransparent(flag);
+                }
+            }
+        }
+    }
+
+    setBlackEdges = (flag) => {
+        this.blackEdges = flag;
+        for (var path in assembly.groups) {
+            for (var obj of assembly.groups[path].children) {
+                if (obj instanceof ObjectGroup) {
+                    obj.setBlackEdges(flag);
+                }
+            }
+        }
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -9,28 +9,128 @@ import { BoundingBox } from './bbox.js'
 const dark = false;
 const bb_factor = 1.0;
 const position = [1, 1, 1];
-const zoom = 1;
+const zoom = 1.2;
 const ortho = true;
 const black_edges = false;
 const edge_color = black_edges ? 0x000000 : 0x707070;
-const transparent = false;
-const shapes = example;
+const transparent = true;
+const transparent_opyacity = 0.5;
+const shapes = example.shapes;
+const mapping = example.mapping;
+const tree = example.tree;
+const bb = example.bb;
 
+
+const defaultDirections = {
+    "front": { "position": [1, 0, 0] },
+    "rear": { "position": [-1, 0, 0] },
+    "top": { "position": [0, 0, 1] },
+    "bottom": { "position": [0, 0, -1] },
+    "left": { "position": [0, 1, 0] },
+    "right": { "position": [0, -1, 0] },
+    "iso": { "position": [1, 1, 1] }
+}
+
+const setCameraPosition = (center, position0) => {
+    var cameraPosition = new THREE.Vector3(...position0).normalize().multiplyScalar(6 * bb_max);
+    cameraPosition = cameraPosition.add(new THREE.Vector3(...center));
+    camera.position.set(...cameraPosition.toArray());
+}
+
+const setOrthoCamera = (ortho_flag) => {
+    if (ortho_flag) {
+        camera.toOrthographic()
+    } else {
+        camera.toPerspective()
+    }
+}
+
+const setOrtho = (e) => {
+    const flag = !!e.target.checked;
+    setOrthoCamera(flag);
+}
+
+const setTransparency = (e) => {
+    const flag = !!e.target.checked;
+    assembly.setTransparent(flag);
+}
+
+const setBlackEdges = (e) => {
+    const flag = !!e.target.checked;
+    assembly.setBlackEdges(flag);
+}
+
+const reset = () => {
+    setCameraPosition(bbox.center, defaultDirections["iso"]["position"])
+    camera.setZoom(zoom);
+    camera.lookAt(bbox.center);
+    controls.target = controls.target0;
+}
+
+const resize = () => {
+    camera.setZoom(zoom);
+}
+
+const setView = (e) => {
+    const dir = defaultDirections[e.target.className]["position"]
+    setCameraPosition(bbox.center, dir);
+}
+
+// render
+const animate = () => {
+    requestAnimationFrame(animate);
+    controls.update();
+    renderer.render(scene, camera);
+}
+
+const dump = (assembly, ind) => {
+    if (ind == undefined) {
+        ind = ""
+    }
+    if (assembly.parts) {
+        for (var part of assembly.parts) {
+            dump(part, ind + "  ");
+        }
+    }
+}
+
+//
+// main
+//
+
+// setup renderer
+const container = document.getElementById('cad_view');
 const renderer = new THREE.WebGLRenderer({
     alpha: !dark,
     antialias: true
 });
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const width = container.clientWidth
+const height = container.clientHeight
+renderer.setSize(width, height);
+container.appendChild(renderer.domElement);
+
+// configure the UI
+document.querySelector('.ortho').addEventListener('change', setOrtho);
+document.querySelector('.ortho').checked = ortho;
+document.querySelector('.reset').addEventListener('click', reset);
+document.querySelector('.resize').addEventListener('click', resize);
+document.querySelector('.transparent').addEventListener('change', setTransparency);
+document.querySelector('.transparent').checked = transparent;
+document.querySelector('.black_edges').addEventListener('change', setBlackEdges);
+document.querySelector('.black_edges').checked = black_edges;
+
+["front", "rear", "top", "bottom", "left", "right", "iso"].forEach((b) => {
+    document.querySelector(`.${b}`).addEventListener('click', setView);
+})
 
 
 // render the assembly
-var assembly = new Assembly(shapes, window.innerWidth, window.innerHeight, edge_color, transparent);
+var assembly = new Assembly(shapes.parts[0], width, height, edge_color, transparent, transparent_opyacity);
 var geom = assembly.render();
 
 // use the provided bounding box
-const bb = new BoundingBox(example.bb)
-const bb_max = bb.max_dist_from_center()
+const bbox = new BoundingBox(bb)
+const bb_max = bbox.max_dist_from_center()
 
 // build the scene
 const scene = new THREE.Scene();
@@ -41,28 +141,23 @@ scene.add(amb_light);
 
 // define the camera
 
-const camera_position = new THREE.Vector3(...position).normalize().multiplyScalar(6 * bb_max);
-
 var camera = new CombinedCamera(
-    window.innerWidth, window.innerHeight, 35,
-    0.1, 12 * bb_factor * bb_max,
-    0.1, 12 * bb_factor * bb_max
+    width, height, 35,
+    0.1, 10 * bb_factor * bb_max,
+    0.1, 10 * bb_factor * bb_max
 )
 camera.up = new THREE.Vector3(0, 0, 1)
-camera.position.set(...camera_position.toArray());
-camera.toOrthographic()
+setCameraPosition(bbox.center, position);
+setOrthoCamera(true);
+resize()
 
 // define the orbit controller
 const controls = new OrbitControls(camera, renderer.domElement);
-
-// render
-function animate() {
-    requestAnimationFrame(animate);
-    controls.update();
-    renderer.render(scene, camera);
-}
 
 animate()
 global.geom = geom
 global.scene = scene
 global.example = example
+global.assembly = assembly
+global.camera = camera
+global.controls = controls
