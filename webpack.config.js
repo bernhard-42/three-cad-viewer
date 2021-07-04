b106f299727c0935230e3ee2eb221153ab4727b8
Bernhard <b_walter@arcor.de>
2021-07-04 16:22:07 +0200

    package updates

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,9 @@
-// const path = require('path');
+const path = require('path');
 
 module.exports = {
     entry: './src/index.js',
-    // output: {
-    //     filename: 'main.js',
-    //     path: path.resolve(__dirname, 'dist'),
-    // },
-    target: false,
-    // plugins: [
-    //     WebExtensionTarget(nodeConfig)
-    // ],
-    resolve: {
-        fallback: {
-            "fs": false,
-            "tls": false,
-            "net": false,
-            "path": false,
-            "zlib": false,
-            "http": false,
-            "https": false,
-            "stream": false,
-            "crypto": false,
-            "crypto-browserify": false,
-            "util": false,
-            "os": false,
-            "buffer": false,
-            "vm": false,
-            "tty": require.resolve("tty-browserify"),
-            "child_process": false,
-            "worker_threads": false,
-            "process": false,
-            "pnpapi": false,
-            "constants": false,
-            "assert": false,
-        }
-    }
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
 };
