e8f14da2c99866d093c27bddc9be7e28b1c74bbc
Bernhard <b_walter@arcor.de>
2021-07-12 08:36:22 +0200

    cleaned up rollup config and added serve + autoreload

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,12 @@
+import process from 'process'
+import os from 'os'
 import resolve from '@rollup/plugin-node-resolve';
 import css from 'rollup-plugin-import-css'
 import svg from 'rollup-plugin-svg-import';
+import { terser } from 'rollup-plugin-terser';
+import serve from 'rollup-plugin-serve'
+import livereload from 'rollup-plugin-livereload'
+
 
 const default_plugins = [
     resolve(),
@@ -8,13 +14,57 @@ const default_plugins = [
     svg({ stringify: true })
 ]
 
-export default {
+const devConfig = {
     input: 'src/index.js',
     plugins: [
-        ...default_plugins
+        ...default_plugins,
+        serve({
+            host: os.hostname(),
+            port: 8082,
+        }),
+        livereload()
     ],
     output: {
-        file: 'dist/three-cad-viewer.module.js',
         format: 'esm',
+        file: 'dist/three-cad-viewer.module.js',
     }
 }
+
+const prodConfig = [
+    {
+        input: 'src/index.js',
+        plugins: [
+            ...default_plugins,
+        ],
+        output: {
+            format: 'esm',
+            file: 'dist/three-cad-viewer.module.js',
+        }
+    },
+    {
+        input: 'src/index.js',
+        plugins: [
+            ...default_plugins
+        ],
+        output: {
+            format: 'umd',
+            name: "CadCViewer",
+            file: 'dist/three-cad-viewer.js',
+            indent: '\t'
+        }
+    },
+    {
+        input: 'src/index.js',
+        plugins: [
+            ...default_plugins,
+            terser(),
+        ],
+        output: {
+            format: 'umd',
+            name: "CadCViewer",
+            file: 'dist/three-cad-viewer.min.js',
+        }
+    },
+];
+
+export default (process.env.BUILD === 'production') ? prodConfig : devConfig
