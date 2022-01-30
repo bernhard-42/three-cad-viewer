8e80f537f9c10d724aef941fc4a2d01e1c49eb55
Bernhard <b_walter@arcor.de>
2022-01-30 15:35:46 +0100

    fix import path

--- src/nestedgroup.js
+++ src/nestedgroup.js
@@ -2,7 +2,7 @@ import * as THREE from "three";
 import { LineSegments2 } from "three/examples/jsm/lines/LineSegments2.js";
 import { LineSegmentsGeometry } from "./patches.js";
 import { LineMaterial } from "three/examples/jsm/lines/LineMaterial.js";
-import { VertexNormalsHelper } from "three/examples/jsm/helpers/VertexNormalsHelper";
+import { VertexNormalsHelper } from "three/examples/jsm/helpers/VertexNormalsHelper.js";
 import { BoundingBox } from "./bbox.js";
 
 class ObjectGroup extends THREE.Group {
