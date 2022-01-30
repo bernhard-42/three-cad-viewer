8935f5fedbdea951b3b2848af95197b266f7ca02
Bernhard <b_walter@arcor.de>
2022-01-30 15:36:03 +0100

    fix linter hints

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
-import process from 'process'
-import os from 'os'
-import resolve from '@rollup/plugin-node-resolve';
-import css from 'rollup-plugin-import-css'
-import svg from 'rollup-plugin-svg-import';
-import { terser } from 'rollup-plugin-terser';
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
-import pkg from './package.json';
+import process from "process";
+import os from "os";
+import resolve from "@rollup/plugin-node-resolve";
+import css from "rollup-plugin-import-css";
+import svg from "rollup-plugin-svg-import";
+import { terser } from "rollup-plugin-terser";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import pkg from "./package.json";
 
 function addMin(name) {
-    return `${name.slice(0, -3)}.min.js`
+    return `${name.slice(0, -3)}.min.js`;
 }
 
 const umdName = "CadViewer";
@@ -18,42 +18,42 @@ const default_plugins = [
     resolve(),
     css(),
     svg({ stringify: true })
-]
+];
 
 var config;
 
-if (process.env.BUILD === 'production') {
+if (process.env.BUILD === "production") {
     config = [
         {
-            input: 'src/index.js',
+            input: "src/index.js",
             plugins: [
                 ...default_plugins,
             ],
             output: [
                 {
-                    format: 'es',
+                    format: "es",
                     file: pkg.module,
                 },
                 {
-                    format: 'umd',
+                    format: "umd",
                     name: umdName,
                     file: pkg.main,
                 },
             ]
         },
         {
-            input: 'src/index.js',
+            input: "src/index.js",
             plugins: [
                 ...default_plugins,
                 terser(),
             ],
             output: [
                 {
-                    format: 'es',
+                    format: "es",
                     file: addMin(pkg.module),
                 },
                 {
-                    format: 'umd',
+                    format: "umd",
                     name: umdName,
                     file: addMin(pkg.main),
                 },
@@ -62,7 +62,7 @@ if (process.env.BUILD === 'production') {
     ];
 } else {
     config = {
-        input: 'src/index.js',
+        input: "src/index.js",
         plugins: [
             ...default_plugins,
             serve({
@@ -72,10 +72,10 @@ if (process.env.BUILD === 'production') {
             livereload()
         ],
         output: {
-            format: 'es',
+            format: "es",
             file: pkg.module,
         }
-    }
+    };
 }
 
-export default config
+export default config;
