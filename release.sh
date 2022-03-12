c940577ee5ca8d19dd78f103aa5932b06b3fa014
Bernhard <b_walter@arcor.de>
2022-03-12 19:20:57 +0100

    added create tag

--- release.sh
+++ release.sh
@@ -3,6 +3,13 @@ CURRENT_VERSION=$(cat package.json | jq -r .version)
 
 echo v$CURRENT_VERSION
 
+echo "pushing this release"
+git tag v$CURRENT_VERSION
+git push
+git push --tags
+
+echo "creating github release"
+
 github-release release -u bernhard-42 -r three-cad-viewer -t v$CURRENT_VERSION -n three-cad-viewer-$CURRENT_VERSION
 sleep 2
 for f in dist/*; do
