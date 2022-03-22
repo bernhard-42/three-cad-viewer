5efb288b016f89e0bff612904211f4f239d904f7
Bernhard <b_walter@arcor.de>
2022-03-22 10:40:46 +0100

    increased sleep duration

--- release.sh
+++ release.sh
@@ -3,15 +3,15 @@ CURRENT_VERSION=$(cat package.json | jq -r .version)
 
 echo v$CURRENT_VERSION
 
-echo "pushing this release"
-git tag v$CURRENT_VERSION
-git push
-git push --tags
+# echo "pushing this release"
+# git tag v$CURRENT_VERSION
+# git push
+# git push --tags
 
 echo "creating github release"
 
 github-release release -u bernhard-42 -r three-cad-viewer -t v$CURRENT_VERSION -n three-cad-viewer-$CURRENT_VERSION
-sleep 2
+sleep 5
 for f in dist/*; do
     echo $f
     github-release upload  -u bernhard-42 -r three-cad-viewer -t v$CURRENT_VERSION -n $(basename $f) -f $f
