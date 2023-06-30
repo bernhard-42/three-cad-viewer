1440a51e868b7cd7f3885fc1a59b4b33a87a610f
Bernhard <b_walter@arcor.de>
2023-06-30 20:06:09 +0200

    exported build dpocs script

---
+++ build_docs.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -fr docs
+mkdir -p docs/dist
+jsdoc -d docs -R Readme.md src/camera.js src/controls.js src/viewer.js src/display.js src/types.js
+cp dist/three-cad-viewer.esm.css dist/three-cad-viewer.esm.js docs/dist
+cp -R examples docs
+cp fonts/helvetiker_regular.typeface.json docs/fonts
+cp index.html docs/example.html
