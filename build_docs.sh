31bea1c4e5fdf458c93b9a067f3ce4cd41d30b28
Bernhard <b_walter@arcor.de>
2023-07-01 08:22:12 +0200

    removed font copy

--- build_docs.sh
+++ build_docs.sh
@@ -5,5 +5,4 @@ mkdir -p docs/dist
 jsdoc -d docs -R Readme.md src/camera.js src/controls.js src/viewer.js src/display.js src/types.js
 cp dist/three-cad-viewer.esm.css dist/three-cad-viewer.esm.js docs/dist
 cp -R examples docs
-cp fonts/helvetiker_regular.typeface.json docs/fonts
 cp index.html docs/example.html
