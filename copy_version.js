87eca58396019992199a0fef4dbd443cc76c84c9
Bernhard <b_walter@arcor.de>
2024-03-09 12:58:11 +0100

    add semicolon

--- copy_version.js
+++ copy_version.js
@@ -1,2 +1,5 @@
 const { version } = require("./package.json");
-require("fs").writeFileSync("./src/_version.js", `export const version = "${version}"\n`);
+require("fs").writeFileSync(
+  "./src/_version.js",
+  `export const version = "${version}";\n`,
+);
