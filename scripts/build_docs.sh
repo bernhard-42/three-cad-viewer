1041cdeb1317aac16c2351f4e78cf51d703acfc0
Bernhard <b_walter@arcor.de>
2025-12-17 11:13:41 +0100

    fix jsdocs call

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -3,7 +3,7 @@ cd "$(dirname "$0")/.."
 
 rm -fr docs
 mkdir -p docs/dist
-jsdoc -d docs -R Readme.md src/camera.js src/controls.js src/viewer.js src/display.js src/types.js
+jsdoc -d docs -R Readme.md src/core/viewer.ts src/core/types.ts src/ui/display.ts src/camera/camera.ts src/camera/controls.ts
 cp dist/three-cad-viewer.css dist/three-cad-viewer.esm.js docs/dist
 cp -R examples docs
 cp index.html docs/example.html
