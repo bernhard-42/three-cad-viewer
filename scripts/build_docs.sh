877c4c1c782bf116bdf1ac8702286f7d52b60448
Bernhard <b_walter@arcor.de>
2026-03-16 14:53:07 +0100

    use yarn to run typedoc

--- scripts/build_docs.sh
+++ scripts/build_docs.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$0")/.."
 
 rm -fr docs
-npx typedoc --out docs --readme Readme.md --entryPoints src/core/viewer.ts src/core/types.ts src/ui/display.ts src/camera/camera.ts src/camera/controls.ts
+yarn typedoc --out docs --readme Readme.md --entryPoints src/core/viewer.ts src/core/types.ts src/ui/display.ts src/camera/camera.ts src/camera/controls.ts
 mkdir -p docs/dist
 cp dist/three-cad-viewer.css dist/three-cad-viewer.esm.js docs/dist
 cp -R examples docs
